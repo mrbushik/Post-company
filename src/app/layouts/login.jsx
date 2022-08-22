f52ee0d77a6e733197cd970c85f9573bba185834
Nikita <mrbushik1@gmail.com>
2022-08-22 11:56:31 +0300

    creating loagin form

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Login() {
-    return <div>Login</div>;
+    const [email, setEmail] = React.useState("");
+    const handleCange = (e) => {
+        setEmail(e.target.value);
+    };
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">email</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => handleCange(e)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">password</label>
+                <input type="password" id="password" />
+            </div>
+        </form>
+    );
 }
 
 export default Login;
