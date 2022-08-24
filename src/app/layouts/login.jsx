7d810b15f79aa6234c247b2bbb044f44ebebc930
Nikita <mrbushik1@gmail.com>
2022-08-24 11:56:28 +0300

    show password btn

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,46 +1,90 @@
-export function validator(data, config) {
-    const errors = {};
-    function validate(validateMethod, data, config) {
-        let statusValidate;
-        switch (validateMethod) {
-            case "isRequired":
-                statusValidate = data.trim() === "";
-                break;
-            case "isEmail": {
-                const emailRegExp = /^\S+@\S+\.\S+$/g;
-                statusValidate = !emailRegExp.test(data);
-                break;
+import React, { useEffect, useState } from "react";
+import TextField from "../components/textField";
+import { validator } from "../utils/validator";
+
+const Login = () => {
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Email введен некорректно"
             }
-            case "isCapitalSymbol": {
-                const capitalRegExp = /[A-Z]+/g;
-                statusValidate = !capitalRegExp.test(data);
-                break;
-            }
-            case "isContainDigit": {
-                const digitRegExp = /\d+/g;
-                statusValidate = !digitRegExp.test(data);
-                break;
-            }
-            case "min": {
-                statusValidate = data.length < config.value;
-                break;
-            }
-            default:
-                break;
-        }
-        if (statusValidate) return config.message;
-    }
-    for (const fieldName in data) {
-        for (const validateMethod in config[fieldName]) {
-            const error = validate(
-                validateMethod,
-                data[fieldName],
-                config[fieldName][validateMethod]
-            );
-            if (error && !errors[fieldName]) {
-                errors[fieldName] = error;
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязателен для заполнения"
+            },
+            isCapitalSymbol: {
+                message: "Пароль должен содержать хотя бы одну заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать хотя бы одно число"
+            },
+            min: {
+                message: "Пароль должен состоять минимум из 8 символов",
+                value: 8
             }
         }
-    }
-    return errors;
-}
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const isValid = Object.keys(errors).length === 0;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Электронная почта"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Пароль"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            className="btn btn-primary w-100 mx-auto"
+                            type="submit"
+                            disabled={!isValid}
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/app/utils/validator.js
+++ src/app/utils/validator.js
@@ -1,23 +1,44 @@
 export function validator(data, config) {
     const errors = {};
     function validate(validateMethod, data, config) {
+        let statusValidate;
         switch (validateMethod) {
             case "isRequired":
-                if (data.trim() === "") return config.massage;
+                statusValidate = data.trim() === "";
                 break;
+            case "isEmail": {
+                const emailRegExp = /^\S+@\S+\.\S+$/g;
+                statusValidate = !emailRegExp.test(data);
+                break;
+            }
+            case "isCapitalSymbol": {
+                const capitalRegExp = /[A-Z]+/g;
+                statusValidate = !capitalRegExp.test(data);
+                break;
+            }
+            case "isContainDigit": {
+                const digitRegExp = /\d+/g;
+                statusValidate = !digitRegExp.test(data);
+                break;
+            }
+            case "min": {
+                statusValidate = data.length < config.value;
+                break;
+            }
             default:
                 break;
         }
+        if (statusValidate) return config.message;
     }
-    for (const fildName in data) {
-        for (const validateMethod in config[fildName]) {
+    for (const fieldName in data) {
+        for (const validateMethod in config[fieldName]) {
             const error = validate(
                 validateMethod,
-                data[fildName],
-                config[fildName][validateMethod]
+                data[fieldName],
+                config[fieldName][validateMethod]
             );
-            if (error && !errors[fildName]) {
-                errors[fildName] = error;
+            if (error && !errors[fieldName]) {
+                errors[fieldName] = error;
             }
         }
     }
